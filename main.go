c9fbbde019c26dbaeb170e3996d8072ef33e5022
jdingus93 <jdingus93@yahoo.com>
2025-05-20 19:05:30 -0500

    removed readiness handler file and placed code in main.go.  Added a state handler and a counter to see how many requests are made

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	
+	fileServer := http.FileServer(http.Dir("."))
+	handler := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := http.Server{
 		Addr:	":8080",
@@ -19,4 +32,28 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
